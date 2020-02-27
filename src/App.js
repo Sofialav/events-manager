46bd6639c94049a53440caa08979a917837b904e
Sofialav <cemeteryvixen@gmail.com>
2020-02-27 15:49:47 +0100

    routing set-up

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Route } from "react-router-dom";
+import Home from "./components/Home";
+
 import "./App.css";
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          <Route path="/" exact component={Home} />
+        </div>
+      </Provider>
+    );
+  }
 }
 
 export default App;
